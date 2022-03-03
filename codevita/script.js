f9cda5b5b9d7e8288a46963f6ae312b0ec9004a5
Kush Shinde <89335622+KushShinde@users.noreply.github.com>
2022-03-03 20:38:01 +0530

    Add files via upload

---
+++ codevita/script.js
@@ -0,0 +1,79 @@
+// // day 1
+// //if -else
+// let a = 10;
+// if (condition){
+//     // success
+// }else{
+// //    failure
+// }
+// // when you have a single conditon to check
+
+// // if-else-ladder
+// if(conditon){
+
+// }else if{
+
+// }else{
+
+// }
+// // // Take age as the variable 
+// // age < 18 = Child
+// // greater than 18 but less than 40 = Adult 
+// // age is greater than 40 = Old age
+ 
+// // var age = 15;
+// // switch(age){
+// //     case (age<18):
+// //         console.log("child");
+// //         break;
+// //         case (18<  age < 40):
+// //         console.log("Adult");
+// //         break;
+// //         default:
+// //         console.log("Old age");
+// //         break;
+
+
+// // }
+//  var day =2;
+//  switch (day) {
+//      case 0:
+//          console.log(" today is monday")
+//          break;
+//          case "1":
+//             console.log(" today is tuesday")
+//             break;
+//          case "2":
+//             console.log(" today is wednesday")
+//                 break;
+//         case "3":
+//             console.log("today is thursday")
+//             break;
+//         case "4":
+//             console.log(" today is friday")
+//             break;
+//         case "5":
+//             console.log(" today is saturday") 
+//             break;
+//         case "6":
+//             console.log(" today is sunday")
+//             break;
+// default:
+//     console.log("please check the number you choose")
+//          break;
+//  }
+
+// loops
+
+// 3 condition
+// inialization
+// declaration the variable
+// increment and decrement statement
+
+// for(let i=0; i<=10; i++){
+//   console.log (i);
+// }
+
+// for(let i= 0; i<=10; i++){
+//     console.log("19 *"+ i + "="+ 19*i)
+// }
